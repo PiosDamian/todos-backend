5b18f9924a480e8403599cc08fead4cd66fb76cc
PiosDamian <piosdamian@gmail.com>
2021-02-25 22:19:18 +0100

    Update models.py

--- todos/models.py
+++ todos/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, default='', null=True)
-    # items są przechowywane jako json listy, uznałem rozwiązanie za wystarczające
+    # items są przechowywane jako json listy, dzięki takiemu podejściu to front decyduje jakiego typu dane chcemy prezentować, edytować itd.
+    # daje nam to elastyczność na froncie
     items = models.TextField(default='[]')
